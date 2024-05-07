e2c4d499496c1fd398b162f560b22be3b22d4567
Omm1s <165832528+Omm1s@users.noreply.github.com>
2024-05-07 16:40:03 +0200

    Slavne osobe

---
+++ Slavne osobe/scripts/app.js
@@ -0,0 +1,47 @@
+function deniroKid(){
+    document.getElementById('deniro').src = './media/deniro-kid.jpg'
+}
+
+function noleKid(){
+    document.getElementById('nole').src = './media/nole-kid.jpg'
+}
+
+function loganKid(){
+    document.getElementById('logan').src = './media/logan-kid.jpeg'
+}
+
+function elonKid(){
+    document.getElementById('elon').src = './media/elon-kid.jpg'
+}
+
+function messiKid(){
+    document.getElementById('messi').src = './media/messi-kid.jpeg'
+}
+
+function cruiseKid(){
+    document.getElementById('cruise').src = './media/cruise-kid.jpg'
+}
+
+function deniroNow(){
+    document.getElementById('deniro').src = './media/deniro-now.jpeg'
+}
+
+function noleNow(){
+    document.getElementById('nole').src = './media/nole-now.jpeg'
+}
+
+function loganNow(){
+    document.getElementById('logan').src = './media/logan-now.jpeg'
+}
+
+function elonNow(){
+    document.getElementById('elon').src = './media/elon-now.jpeg'
+}
+
+function messiNow(){
+    document.getElementById('messi').src = './media/messi-now.jpg'
+}
+
+function cruiseNow(){
+    document.getElementById('cruise').src = './media/cruise-now.jpeg'
+}
